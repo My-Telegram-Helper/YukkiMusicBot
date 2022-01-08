b2e58719b313e4325e3ffce94ea54e0728425c56
King Amda <86409667+King-Amda@users.noreply.github.com>
2022-01-08 12:28:00 +0530

    Update Downloader.py

--- Yukki/Core/PyTgCalls/Downloader.py
+++ Yukki/Core/PyTgCalls/Downloader.py
@@ -70,7 +70,7 @@ def download(videoid: str, mystic, title) -> str:
                 taken = "00:00"
             size = d["_total_bytes_str"]
             mystic.edit(
-                f"**{MUSIC_BOT_NAME} Downloader**\n\n**Title:** {title[:50]}:\n\n100% ████████████100%\n\n**Time Taken:** {taken} Seconds\n\nConverting Audio[FFmpeg Process]"
+                f"**{MUSIC_BOT_NAME} Downloader**\n\n**Title:** {title[:50]}:\n\n100% ████████████100%\n\n**Time Taken:** {taken} Seconds\n\nConverting Audio [Processing...]"
             )
 
     ydl_optssx = {
